33368fe57a6a541b1b1f83c5410195d41b3da87d
tuananhnp <tuananhnp1001@gmail.com>
2021-04-21 15:21:28 +0200

    new

--- js/nav.slide.down.transition.js
+++ js/nav.slide.down.transition.js
@@ -1,3 +1,9 @@
 $('.navbar-toggler').click(function() {
-  $('#navbar4').slideToggle(300);	
+  $('#navbar4').slideToggle(300);
+  fullpage_api.setMouseWheelScrolling(true);
+});
+
+$('.why-signmylogo').click(function() {
+  $('#navbar4').slideToggle(300);
+  $('.animated-icon').toggleClass('open');
 });
